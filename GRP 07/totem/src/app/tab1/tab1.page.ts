e9a0ff64f29b03bb69a5fca6d3af985eeeee21a8
Luann Lucas <105380186+heyluannlucas@users.noreply.github.com>
2023-10-25 21:55:27 -0300

    senhas

--- GRP 07/totem/src/app/tab1/tab1.page.ts
+++ GRP 07/totem/src/app/tab1/tab1.page.ts
@@ -24,52 +24,32 @@ export class Tab1Page {
   proximasSenhas: Senha[] = [];
   contadores: { [tipo: string]: number } = {};
   senhaAtual: Senha | null = null;
-  filaProximasSenhas: Senha[] = [];
 
   constructor(public senhaService: SenhaService) {}
 
   gerarSenha(tipo: string) {
-    this.senhaService.novaSenha(tipo);
+    this.senhaService.novaSenha(tipo); 
     const dataAtual = new Date().toLocaleDateString('pt-BR');
-
+  
     let ordem: string = '00';
-
+  
     if (this.contadores[tipo]) {
       ordem = (this.contadores[tipo] + 1).toString().padStart(2, '0');
     }
-
-    const novaSenha = new Senha(dataAtual, tipo, ordem);
-
-    if (!this.senhaAtual) {
-      // Se não houver senha atual, defina a nova senha como a senha atual.
-      this.senhaAtual = novaSenha;
-    } else {
-      // Senão, adicione a nova senha à fila de próximas senhas.
-      this.filaProximasSenhas.push(novaSenha);
-
-      if (this.filaProximasSenhas.length > 5) {
-        this.filaProximasSenhas.shift();
-      }
+  
+    this.senhaAtual = this.proximasSenhas.length > 0 ? this.proximasSenhas[0] : null;
+    this.proximasSenhas.push(new Senha(dataAtual, tipo, ordem));
+    
+    if (this.proximasSenhas.length > 5) {
+      this.proximasSenhas.shift();
     }
-
+  
     this.contadores[tipo] = this.contadores[tipo] ? this.contadores[tipo] + 1 : 1;
   }
 
   avancarProximaSenha() {
-    if (this.filaProximasSenhas.length > 0) {
-      // Move a senha atual para uma variável temporária
-      const senhaTemp = this.senhaAtual;
-
-      // Remove a senha atual da fila de próximas senhas
-      this.filaProximasSenhas.shift();
-
-      // Adicione a senha temporária de volta à fila de próximas senhas
-      if (senhaTemp) {
-        this.filaProximasSenhas.push(senhaTemp);
-      }
-
-      // Defina a próxima senha na fila como a senha atual
-      this.senhaAtual = this.filaProximasSenhas[0];
+    if (this.proximasSenhas.length > 0) {
+      this.senhaAtual = this.proximasSenhas.shift() as Senha;
     }
   }
 }
