bffc6427b1fcbfbc985458f709b91b8edc1cb65f
Flavia Souza <wdflaviia@gmail.com>
2023-11-10 20:18:20 -0300

    primeira tela feita - lista 5

---
+++ GRP 03/PokemonApp/src/app/tab1/tab1.page.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: '',
+  };
+  
+  pokemon: any = {
+    name: '',
+    abilities: [],
+    height: 0,
+    weight: 0,
+    imageUrl: ''
+  };
+  
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService
+  ) {}
+  buscarPokemon() {
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+    .subscribe((value) => {
+      this.areaBusca.logradouro = JSON.parse(JSON.stringify(value)) ['logradouro'];
+      this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value)) ['bairro'];
+      this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value)) ['localidade'];
+      this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value)) ['uf'];
+    });
+
+    const pokemonId = Math.floor(Math.random() * 100) + 1;
+
+    this.pokeAPIService.getPokeAPIService(pokemonId)
+    .subscribe((pokemonData: any) => {
+      this.pokemon.name = pokemonData.name.toUpperCase();
+      this.pokemon.abilities = pokemonData.abilities.map((ability: any) => ability.ability.name);
+      this.pokemon.height = pokemonData.height;
+      this.pokemon.weight = pokemonData.weight;
+      this.pokemon.imageUrl = pokemonData.sprites.other.dream_world.front_default;
+    });
+    
+  };
+
+}
